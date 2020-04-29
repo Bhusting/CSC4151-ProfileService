9ec278973b68334ae95f172668e9a2cf47338f90
Bhusting97 <bhusting@spu.edu>
2020-04-29 15:52:54 -0700

    Added Domain Library and Profile Class.

---
+++ Domain/Profile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Profile
+    {
+        public Guid UserId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+        
+        public string XP { get; set; }
+
+        public Guid HouseId { get; set; }
+    }
+}
