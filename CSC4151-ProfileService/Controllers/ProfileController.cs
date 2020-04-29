61872c446c5526dece0f7c3270f35c01b67809bd
Bhusting97 <bhusting@spu.edu>
2020-04-29 15:54:35 -0700

    Added GetProfile Controller Method and Profile Controller.

---
+++ CSC4151-ProfileService/Controllers/ProfileController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Common.Repositories;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CSC4151_ProfileService.Controllers
+{
+    [ApiController]
+    [Route("Profile")]
+    public class ProfileController : ControllerBase
+    {
+        private readonly ILogger<ProfileController> _logger;
+        private readonly IProfileRepository _profileRepository;
+
+        public ProfileController(ILogger<ProfileController> logger, IProfileRepository profileRepository)
+        {
+            _logger = logger;
+            _profileRepository = profileRepository;
+        }
+
+        /// <summary>
+        /// Retrieves a Profile from SQL.
+        /// </summary>
+        /// <param name="id">Id of the profile to retrieve.</param>
+        /// <returns>User Profile</returns>
+        [HttpGet("{id}")]
+        public async Task<Profile> GetProfile(Guid id)
+        {
+            _logger.LogInformation($"Get Profile {id}");
+
+            var profile = await _profileRepository.GetProfile(id);
+
+            return profile;
+        }
+    }
+}
