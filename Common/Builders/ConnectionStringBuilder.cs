79bc4d3a049d5788f0e6caf4f66a03fc2248bae9
Bhusting97 <bhusting@spu.edu>
2020-04-29 15:53:34 -0700

    Added ConnectionStringBuilder.

---
+++ Common/Builders/ConnectionStringBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Settings;
+
+namespace Common.Builders
+{
+    public static class ConnectionStringBuilder
+    {
+
+        public static string BuildSQLConnectionString(SqlSettings sqlSettings)
+        {
+            return $"Server=tcp:{sqlSettings.Server},1433;Initial Catalog={sqlSettings.Catalog};Persist Security Info=False;User ID={sqlSettings.User};Password={sqlSettings.Password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        }
+
+    }
+}
