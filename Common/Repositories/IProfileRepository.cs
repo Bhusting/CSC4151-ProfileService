9dee743763750be9d626b42c75cc8aaeb57f0358
Bhusting97 <bhusting@spu.edu>
2020-04-29 15:54:06 -0700

    Added Common Library and ProfileRepository.

---
+++ Common/Repositories/IProfileRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Common.Repositories
+{
+    public interface IProfileRepository
+    {
+        /// <summary>
+        /// Retrieves a profile from the Profile Table.
+        /// </summary>
+        /// <param name="id">Id of the Profile to retrieve.</param>
+        /// <returns>User Profile</returns>
+        Task<Profile> GetProfile(Guid id);
+
+
+    }
+}
---
+++ Common/Repositories/ProfileRepository.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using Common.Builders;
+using Common.Clients;
+using Common.Settings;
+using Domain;
+using SqlCommandBuilder = Common.Builders.SqlCommandBuilder;
+
+namespace Common.Repositories
+{
+    public class ProfileRepository : IProfileRepository
+    {
+
+        private readonly SqlClient _sqlClient;
+
+        public ProfileRepository(SqlClient sqlClient)
+        {
+            _sqlClient = sqlClient;
+        }
+
+        /// <summary>
+        /// Retrieves a profile from the Profile Table.
+        /// </summary>
+        /// <param name="id">Id of the Profile to retrieve.</param>
+        /// <returns>User Profile</returns>
+        public async Task<Profile> GetProfile(Guid id)
+        {
+            var cmd = SqlCommandBuilder.GetIndividualRecordBuilder(typeof(Profile), id);
+
+            var profile = await _sqlClient.Get<Profile>(cmd);
+
+            return profile[0];
+        }
+    }
+}
