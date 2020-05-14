092bb7d6487d97e16e7a0a8f256cd68cdf004b49
Bhusting97 <bhusting@spu.edu>
2020-05-13 17:21:43 -0700

    Added EndToEndTest for Getting Profile by Email.

--- EndToEndTests/UnitTest1.cs
+++ EndToEndTests/UnitTest1.cs
@@ -26,7 +26,24 @@ namespace EndToEndTests
             Assert.True(profile.FirstName == "Rodney");
         }
 
+        [Fact]
+        public async Task GetProfileByEmail()
+        {
+            var httpClient = new HttpClient() { BaseAddress = new Uri("http://localhost:54281") };
 
+            var str = "Profile/Email/theruxter18@hotmail";
+
+            var res = await httpClient.GetAsync(str);
+
+            Assert.True(res.IsSuccessStatusCode);
+
+            var body = await res.Content.ReadAsStringAsync();
+
+            var profile = JsonConvert.DeserializeObject<Profile>(body);
+
+            Assert.True(profile.ProfileId == Guid.Empty);
+            Assert.True(profile.FirstName == "Rodney");
+        }
 
     }
 }
