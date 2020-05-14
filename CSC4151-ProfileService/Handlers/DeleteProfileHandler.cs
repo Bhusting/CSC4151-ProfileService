483ce3b3838590bf035caf2ed49cade595688a68
Bhusting97 <bhusting@spu.edu>
2020-05-13 17:33:22 -0700

    Added DeleteProfileHandler

---
+++ CSC4151-ProfileService/Handlers/DeleteProfileHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common.Builders;
+using Common.Clients;
+using Domain;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace CSC4151_ProfileService.Handlers
+{
+    public class DeleteProfileHandler : IMessageHandler
+    {
+        private readonly ILogger<DeleteProfileHandler> _logger;
+        private readonly SqlClient _sqlClient;
+
+        public DeleteProfileHandler(ILogger<DeleteProfileHandler> logger, SqlClient sqlClient)
+        {
+            _logger = logger;
+            _sqlClient = sqlClient;
+        }
+
+        public async Task Handle(string messageBody)
+        {
+            _logger.LogInformation("Creating User");
+
+            var profileId = JsonConvert.DeserializeObject<Guid>(messageBody);
+
+            var cmd = SqlCommandBuilder.DeleteRecord(typeof(Profile), profileId);
+
+            await _sqlClient.Delete(cmd);
+        }
+    }
+}
