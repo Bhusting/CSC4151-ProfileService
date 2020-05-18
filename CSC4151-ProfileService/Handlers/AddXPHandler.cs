ec02b1329218bed6269c5b352b9fe46ff1f45d1d
Bhusting97 <bhusting@spu.edu>
2020-05-17 17:29:00 -0700

    Added AddXpHandler and UpdateHouseHandler.

---
+++ CSC4151-ProfileService/Handlers/AddXPHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Common.Clients;
+using Common.Repositories;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace CSC4151_ProfileService.Handlers
+{
+    public class AddXPHandler : IMessageHandler
+    {
+        private readonly ILogger<AddXPHandler> _logger;
+        private readonly SqlClient _sqlClient;
+        private readonly IProfileRepository _profileRepository;
+
+        public AddXPHandler(ILogger<AddXPHandler> logger, SqlClient sqlClient, IProfileRepository profileRepository)
+        {
+            _logger = logger;
+            _sqlClient = sqlClient;
+            _profileRepository = profileRepository;
+        }
+
+        public async Task Handle(string messageBody)
+        {
+            var profileId = JsonConvert.DeserializeObject<Guid>(messageBody);
+
+            _logger.LogInformation($"Updating User XP {profileId}");
+
+            var profile = await _profileRepository.GetProfile(profileId);
+
+            var xp = profile.XP + 1;
+
+            var cmd = $"UPDATE Profile SET XP = {xp} WHERE ProfileId = \'{profileId}\'";
+
+            await _sqlClient.Update(cmd);
+        }
+    }
+}
---
+++ CSC4151-ProfileService/Handlers/UpdateHouseHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common.Builders;
+using Common.Clients;
+using Domain;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace CSC4151_ProfileService.Handlers
+{
+    public class UpdateHouseHandler : IMessageHandler
+    {
+        private readonly ILogger<UpdateHouseHandler> _logger;
+        private readonly SqlClient _sqlClient;
+
+        public UpdateHouseHandler(ILogger<UpdateHouseHandler> logger, SqlClient sqlClient)
+        {
+            _logger = logger;
+            _sqlClient = sqlClient;
+        }
+
+        public async Task Handle(string messageBody)
+        {
+            var update = JsonConvert.DeserializeObject<KeyValuePair<Guid, Guid>>(messageBody);
+            
+            _logger.LogInformation($"Updating User House {update.Value}");
+
+            var cmd = $"UPDATE Profile SET HouseId = \'{update.Value}\' WHERE ProfileId = \'{update.Key}\'";
+
+            await _sqlClient.Update(cmd);
+
+        }
+    }
+}
